a57e09ae97064d321d1f4e7ef8a92d576aa84603
Sophia Pearson <codergal89@gmail.com>
2020-10-23 23:41:41 +0200

    support: add basic maybe type skeleton

---
+++ include/sexyparse/maybe.hpp
@@ -0,0 +1,81 @@
+#ifndef SEXYPARSE_MAYBE_HPP_
+#define SEXYPARSE_MAYBE_HPP_
+
+#include <variant>
+#include <system_error>
+#include <type_traits>
+#include <utility>
+#include <functional>
+
+namespace sexy
+{
+
+    namespace internal
+    {
+        struct maybe_error_tag
+        {
+        };
+
+        struct maybe_value_tag
+        {
+        };
+    } // namespace internal
+
+    using bad_maybe_access = std::bad_variant_access;
+
+    template <typename ValueType>
+    struct maybe
+    {
+        using value_type = ValueType;
+        using error_type = std::error_code;
+
+        constexpr explicit maybe(ValueType const &value) : m_underlying{value}
+        {
+        }
+
+        constexpr explicit maybe(ValueType &&value) : m_underlying{std::move(value)}
+        {
+        }
+
+        explicit maybe(std::error_code error) : m_underlying{std::move(error)}
+        {
+        }
+
+        constexpr operator bool() const noexcept
+        {
+            return std::holds_alternative<value_type>(m_underlying);
+        }
+
+        constexpr explicit operator value_type() const
+        {
+            return std::get<value_type>(m_underlying);
+        }
+
+        explicit operator error_type() const noexcept
+        {
+            if (std::holds_alternative<error_type>(m_underlying))
+            {
+                return std::get<error_type>(m_underlying);
+            }
+            return std::error_code{};
+        }
+
+        template <typename Callable>
+        auto constexpr map(Callable &&callable) -> std::enable_if_t<std::is_invocable_v<Callable, value_type>,
+                                                                    maybe<std::invoke_result_t<Callable, value_type>>>
+        {
+            using result_type = std::invoke_result_t<Callable, value_type>;
+            if (*this)
+            {
+                return maybe<result_type>{std::invoke(std::forward<Callable>(callable), std::get<value_type>(m_underlying))};
+            }
+            return maybe<result_type>{std::get<error_type>(m_underlying)};
+        }
+
+    private:
+        std::variant<value_type, error_type> m_underlying;
+    };
+
+} // namespace sexy
+
+#endif
---
+++ test/source/maybe_tests.cpp
@@ -0,0 +1,80 @@
+#include "sexyparse/maybe.hpp"
+
+#include "catch2/catch.hpp"
+
+#include <string>
+#include <system_error>
+
+using namespace std::string_literals;
+
+TEST_CASE("A 'maybe' can be constructed with a value")
+{
+    sexy::maybe{"Test Value"s};
+}
+
+TEST_CASE("A 'maybe' can be constructed with a constant value")
+{
+    auto const value = "Constant Value"s;
+    sexy::maybe{value};
+}
+
+TEST_CASE("A 'maybe' can be constructed with an error code")
+{
+    sexy::maybe<long>{make_error_code(std::errc::no_lock_available)};
+}
+
+TEST_CASE("A 'maybe' constructed with a value converts to true")
+{
+    REQUIRE(static_cast<bool>(sexy::maybe{42}));
+}
+
+TEST_CASE("A 'maybe' constructed with an error converts to false")
+{
+    REQUIRE(!static_cast<bool>(sexy::maybe<float>{make_error_code(std::errc::invalid_argument)}));
+}
+
+TEST_CASE("A 'maybe' constructed with a value converts to the same value")
+{
+    REQUIRE(static_cast<double>(sexy::maybe{1337.0}) == 1337.0);
+}
+
+TEST_CASE("Trying to convert a 'maybe' constructed with an error throws sexy::bad_maybe_access")
+{
+    REQUIRE_THROWS_AS(static_cast<char>(sexy::maybe<char>{make_error_code(std::errc::io_error)}), sexy::bad_maybe_access);
+}
+
+TEST_CASE("A 'maybe' constructed with an error converts to the same error")
+{
+    REQUIRE(static_cast<std::error_code>(sexy::maybe<unsigned>{make_error_code(std::errc::no_link)}) == std::errc::no_link);
+}
+
+TEST_CASE("Converting a 'maybe' constructed with a value yields the empty error code")
+{
+    REQUIRE(static_cast<std::error_code>(sexy::maybe{10ull}) == std::error_code{});
+}
+
+TEST_CASE("Mapping over a 'maybe' constructed with a value calls the supplied function")
+{
+    auto did_call_function{false};
+    sexy::maybe{42}.map([&](auto value) { did_call_function = true; return value; });
+    REQUIRE(did_call_function);
+}
+
+TEST_CASE("Mapping over a 'maybe' constructed with a value yields the supplied function's result in a new maybe")
+{
+    auto result = sexy::maybe{'c'}.map([](auto) { return 42; });
+    REQUIRE(static_cast<int>(result) == 42);
+}
+
+TEST_CASE("Mapping over a 'maybe' constructed with an error does not call the supplied function")
+{
+    auto did_call_function{false};
+    sexy::maybe<wchar_t>{make_error_code(std::errc::address_family_not_supported)}.map([&](auto value) { did_call_function = true; return value; });
+    REQUIRE(!did_call_function);
+}
+
+TEST_CASE("Mapping over a 'maybe' constructed with an error yield a maybe with the same error")
+{
+    auto result = sexy::maybe<int>{make_error_code(std::errc::argument_out_of_domain)}.map([](auto value) { return value; });
+    REQUIRE(static_cast<std::error_code>(result) == std::errc::argument_out_of_domain);
+}
